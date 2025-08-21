ced393db24630492cd307db10cb4f8c8551cae31
apujiono <36912631+apujiono@users.noreply.github.com>
2025-08-21 22:39:07 +0700

    Create PaywallMedia.js

---
+++ frontend/src/components/PaywallMedia.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function PaywallMedia({ username, token }) {
+  return (
+    <div className="p-4 bg-gray-800 rounded-lg">
+      <h2 className="text-xl mb-4">Konten Berbayar</h2>
+      <p>Coming soon: Akses foto/video eksklusif dengan subscription.</p>
+    </div>
+  );
+}
+
+export default PaywallMedia;
