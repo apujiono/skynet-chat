5ce2d5729ba01b553518f52458696f9279d80100
apujiono <36912631+apujiono@users.noreply.github.com>
2025-08-21 22:37:30 +0700

    Create FriendsList.js

---
+++ frontend/src/components/FriendsList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function FriendsList({ username, token }) {
+  return (
+    <div className="p-4 bg-gray-800 rounded-lg">
+      <h2 className="text-xl mb-4">Daftar Teman</h2>
+      <p>Coming soon: Follow/unfollow teman.</p>
+    </div>
+  );
+}
+
+export default FriendsList;
